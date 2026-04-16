6564ca40c0d1f3e65db3d89daf472ffc2c5d641f
romannst <romanstrizzi8@gmail.com>
2026-04-16 16:16:03 -0300

    Resolución de los ejercicios 2, 3 y 4

--- tp2-concurrencia/ejercicios-parte5.sql
+++ tp2-concurrencia/ejercicios-parte5.sql
@@ -27,3 +27,38 @@ BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 INSERT INTO auditoria_log (accion, datos) SELECT 'REPORTE', SUM(saldo) FROM billetera_clientes;
 
 COMMIT;
+
+-- Se utiliza REPEATABLE READ para obtener una "foto" de los datos
+-- en un instante del tiempo (snapshot), evitando lecturas inconsistentes.
+-- No se usan bloqueos explícitos (B2F), porque en este nivel de
+-- aislamiento se garantiza consistencia sin bloquear a otros usuarios (MVCC).
+
+--- EJERCICIO 3
+
+BEGIN;
+
+SELECT saldo FROM billetera_clientes WHERE id_cliente = 4 FOR UPDATE;
+UPDATE billetera_clientes SET saldo = saldo - 10000 WHERE id_cliente = 4 AND saldo >= 10000;
+INSERT INTO auditoria_log (accion, datos) VALUES ('COMPRA', 'Cliente 4 realizó una compra por 10000');
+
+COMMIT;
+
+-- Se utiliza un bloqueo explícito (SELECT ... FOR UPDATE) para implementar
+-- control pesimista (B2F). Esto asegura que la fila del cliente quede bloqueada
+-- mientras dura la transacción, forzando a la segunda pestaña a esperar.
+-- De esta forma se evita el problema de doble gasto en alta concurrencia.
+
+-- EJERCICIO 4
+
+BEGIN;
+
+SELECT saldo FROM billetera_clientes WHERE id_cliente = 2 FOR UPDATE;
+SELECT saldo FROM billetera_clientes WHERE id_cliente = 5 FOR UPDATE;
+UPDATE billetera_clientes SET saldo = saldo - 500 WHERE id_cliente = 5 ;
+UPDATE billetera_clientes SET saldo = saldo + 500 WHERE id_cliente = 2;
+INSERT INTO auditoria_log (accion, datos) VALUES ('TRANSFERENCIA', 'Transferencia de $500 de cliente 5 a cliente 2')
+
+COMMIT;
+
+-- Se utiliza bloqueo explícito (SELECT ... FOR UPDATE) para asegurar un orden consistente
+-- de adquisición de locks (ID menor → mayor), evitando deadlocks en escenarios concurrentes.
