a97377f1159f1974d07b916ef682f3dc39d455b7
romannst <romanstrizzi8@gmail.com>
2026-04-21 17:13:12 -0300

    Ejercicios 2, 3 y 4 terminados

--- tp4-administracion-seguridad/ejercicios.sql
+++ tp4-administracion-seguridad/ejercicios.sql
@@ -38,4 +38,26 @@ FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
 );
 
 INSERT INTO confidencial (id_cliente, telefono_emergencia_cifrado, pin_entrega_hash)
-VALUES ()
+VALUES (1, pgp_sym_encrypt('555-0199','secreto_empresa'), crypt('4092', gen_salt('bf')));
+
+--verificar pin correcto
+SELECT (pin_entrega_hash = crypt('4092', pin_entrega_hash))
+FROM confidencial WHERE id_cliente = 1;
+--recuperar el telefono cifrado
+SELECT (pgp_sym_decrypt(telefono_emergencia_cifrado,'secreto_empresa')) AS telefono_emg_decrypt
+FROM confidencial WHERE id_cliente = 1;
+
+-- Si un empleado con permisos de lectura general hace una copia de la base de datos y se la lleva, no podrá tener acceso a los teléfonos de emergencia ni a los pines de cada cliente, porque su contenido se encuentra cifrado y no tiene la clave de cifrado ni el hash para poder descifrarlo o verificarlo. Esto hace que la información confidencial esté protegida incluso si se accede a la base de datos sin autorización.
+
+-- EJERCICIO 3
+CREATE ROLE rol_vendedor;
+GRANT SELECT,INSERT ON pedidos TO rol_vendedor;
+GRANT UPDATE (estado) ON pedidos TO rol_vendedor;
+
+-- EJERCICIO 4
+REVOKE rol_gerente FROM gerente_ana;
+DROP USER gerente_ana;
+
+-- Si intentamos hacer un DROP USER gerente_ana; directamente sin revocar sus permisos previamente, PostgreSQL lanza un error.
+-- El motor de base de datos tiene este comportamiento defensivo para evitar inconsistencias y problemas de seguridad en la base de datos. Si se permitiera eliminar un usuario sin revocar sus permisos, se podrían generar situaciones donde los permisos otorgados a ese usuario quedarían huérfanos, es decir, sin un propietario válido. Esto podría llevar a que otros usuarios o roles hereden esos permisos de manera no intencionada, lo que podría resultar en accesos no autorizados a datos sensibles o en la ejecución de acciones que el usuario eliminado tenía permitido realizar.
+-- En resumen, el motor de base de datos protege la integridad y la seguridad de la matriz de acceso (DAC) al requerir que se revoquen los permisos antes de eliminar un usuario, asegurando que no queden permisos huérfanos y que la gestión de la seguridad se mantenga clara y consistente.
